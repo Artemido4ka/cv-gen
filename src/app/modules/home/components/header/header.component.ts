2187a69aa75217165811986c6a6899507865457e
artsiomkarenka <>
2024-04-26 19:38:42 +0300

    fix

--- src/app/modules/home/components/header/header.component.ts
+++ src/app/modules/home/components/header/header.component.ts
@@ -1,13 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { RoutingPaths } from 'src/app/shared/constants/routing-paths';
 
 @Component({
   selector: 'cv-gen-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
+  constructor(private translate: TranslateService) {}
+
   homeRoute = RoutingPaths.HOME;
 
+  handleTranslate() {
+    const currentLang = this.translate.currentLang;
+    this.translate.use(currentLang === 'en' ? 'ru' : 'en');
+  }
 }
